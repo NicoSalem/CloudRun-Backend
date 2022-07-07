35a5a254537163469115d82143f7949ce6b182f9
nicholas Salem <nsalem@u.rochester.edu>
2022-07-07 19:36:43 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express();
+const https = require('https');
+
+
+app.get("/", function(req, res) {
+    res.send("hello from the backend");
+});
+
+app.get("/test", function(req, res) {
+    res.sendFile(__dirname + "/test.html");
+});
+
+app.get("/j", function(req, res) {
+    res.json({"key" : "value"})
+});
+
+app.listen(8080, function() {
+    console.log("Server is listening in port 8080")
+});
---
+++ terraform.tf
@@ -0,0 +1,34 @@
+
+provider "google" {
+  project     = "groovy-autumn-290918"
+}
+
+resource "google_cloud_run_backend_service" "default" {
+  name     = "cloudrun-backend"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-central1-docker.pkg.dev/groovy-autumn-290918/app1-repo/cloudrun-back-1:tag1"
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = "us-central1"
+  project     = "groovy-autumn-290918"
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
