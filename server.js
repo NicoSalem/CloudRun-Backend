c049b248a1ef9ba4a74caf896307398080069a89
nicholas Salem <nsalem@u.rochester.edu>
2022-08-02 11:12:58 -0400

    first commit

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const cors          = require('cors')
 app.use(cors())
 app.use(useragent.express())
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.urlencoded({extended: true}));
 
 
 var port = process.env.PORT || 8080;
